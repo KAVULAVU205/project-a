7f1ce1445d6592831e5dc00907b900d3a28fdd3c
RICKSAM-KE <samuelkavulavu@gmail.com>
2025-09-13 13:22:28 +0300

    chapter 12

--- nextjs-dashboard/app/lib/action.ts
+++ nextjs-dashboard/app/lib/action.ts
@@ -16,6 +16,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({id:true,date:true});
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
  
 export async function createInvoice(formData: FormData) {
@@ -23,7 +24,7 @@ export async function createInvoice(formData: FormData) {
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  });
+  });}
 
 
 export async function updateInvoice(id: string, formData: FormData) {
@@ -34,6 +35,10 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
 
 
+
+
+
+
  const amountInCents = amount * 100;
  const date = new Date().toISOString().split('T')[0];
 
@@ -42,8 +47,15 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
-
 }
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
+
+
+
 
 
   
